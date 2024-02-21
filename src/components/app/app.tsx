c3ae139f282b4c58e5e73b1d14d5510683012dcd
Алина Вдовиченко <alinavdovichenko.ru@gmail.com>
2024-02-21 17:43:59 +0500

    3.1. Добавит маршрутизацию.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,13 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {HelmetProvider} from 'react-helmet-async';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import MainPage from '../../pages/main-page/mainPage';
+import FavoritesPage from '../../pages/favorites-page/favoritesPage';
+import OfferPage from '../../pages/offer-page/offerPage';
+import LoginPage from '../../pages/login-page/loginPage';
+import NotFoundPage from '../../pages/not-found-page/NotFoundPage';
+import PrivateRoute from '../private-route/privateRoute';
+
 
 type AppPageProps = {
   placesCount: number;
@@ -6,7 +15,38 @@ type AppPageProps = {
 
 function App({placesCount}: AppPageProps): JSX.Element {
   return (
-    <MainPage placesCount={placesCount} />
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={AppRoute.Main}
+            element={<MainPage placesCount={placesCount} />}
+          />
+          <Route
+            path={AppRoute.Favorites}
+            element={
+              <PrivateRoute
+                authorizationStatus={AuthorizationStatus.NoAuth}
+              >
+                <FavoritesPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path={AppRoute.Login}
+            element={<LoginPage />}
+          />
+          <Route
+            path={AppRoute.Offer}
+            element={<OfferPage />}
+          />
+          <Route
+            path="*"
+            element={<NotFoundPage />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
