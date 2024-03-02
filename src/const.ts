bac4c0060db8eb74e6bc78f6e025f86f210893da
Алина Вдовиченко <alinavdovichenko.ru@gmail.com>
2024-03-02 14:39:23 +0500

    Заменит маркеры на карте

--- src/const.ts
+++ src/const.ts
@@ -17,9 +17,9 @@ export enum AuthorizationStatus {
 }
 
 export const URL_MARKER_DEFAULT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+  'img/pin.svg';
 
 export const URL_MARKER_CURRENT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+  'img/pin-active.svg';
 
 export const handleStars = (width: number) => `${String(Math.round(width) * 20)}%`;
