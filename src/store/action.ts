b75fa299b8a3501402d0a36aca2559cb648ab285
Алина Вдовиченко <alinavdovichenko.ru@gmail.com>
2024-03-13 13:02:59 +0500

    Сконфигурирует пакет axios,  подключит redux-thunk, добавит модуль для работы с токеном.

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,6 +1,7 @@
 import {createAction} from '@reduxjs/toolkit';
 import {CityMap} from '../types/cityMap';
 import {SortType} from '../const';
+import {Offers} from '../types/offer';
 
 export const setCityActive = createAction('main/CityActive', (value: string)=>({payload: value}));
 
@@ -11,3 +12,5 @@ export const setChangeMap = createAction('map/ChangeMap', (value: CityMap)=>({pa
 export const getSortType = createAction('main/SortType', (value: SortType)=>({payload: value}));
 
 export const setSorting = createAction('setSorting');
+
+export const loadOffers = createAction<Offers>('data/loadOffers');
